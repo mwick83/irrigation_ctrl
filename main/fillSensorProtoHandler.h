9b72a05329770a02326359e3c876cc4f035eb694
Manuel Wick <manuel@matronix.de>
2018-01-07 18:20:56 +0100

    Added proper mutex locking to FillSensorProtoHandler

--- main/fillSensorProtoHandler.h
+++ main/fillSensorProtoHandler.h
@@ -34,12 +34,24 @@ private:
 
     uint8_t txBuffer[maxPacketDataLen+1];
 
+    SemaphoreHandle_t requestMutex;
+    StaticSemaphore_t requestMutexBuf;
+
     enum {
         PROTO_TYPE_FILL_LEVEL_REQ = 0x01,
         PROTO_TYPE_FILL_LEVEL_IND = 0x81
     } PROTO_TYPE_E;
 
 public:
+    // This constructor shouldn't be used, but provide minimum init to be safe
+    FillSensorProtoHandler(void)
+    {
+        packetizerInitialized = false;
+        this->packetizer = nullptr;
+
+        ESP_LOGE(logTag, "Unsupported default constructor called!");
+    }
+
     FillSensorProtoHandler(PacketizerClass* packetizer)
     {
         packetizerInitialized = false;
@@ -47,12 +59,14 @@ public:
 
         memset(txBuffer, 0x00, maxPacketDataLen+1);
 
+        requestMutex = xSemaphoreCreateMutexStatic(&requestMutexBuf);
+
         rxPacketQueue = packetizer->getRxPacketQueue();
 
         if(NULL == rxPacketQueue) {
-            ESP_LOGE(LOG_TAG_FILL_PROTO, "Couldn't get rx packet queue handle from packetizer!");
+            ESP_LOGE(logTag, "Couldn't get rx packet queue handle from packetizer!");
         } else {
-            ESP_LOGD(LOG_TAG_FILL_PROTO, "Packetizer setup ok.");
+            ESP_LOGD(logTag, "Packetizer setup ok.");
             packetizerInitialized = true;
         }
     }
@@ -63,27 +77,38 @@ public:
         int fillLevel = -1;
 
         //TickType_t wait = portMAX_DELAY;
-        TickType_t wait = pdMS_TO_TICKS(1000);
+        TickType_t wait = pdMS_TO_TICKS(500);
 
         if(!packetizerInitialized) return -1;
 
-        txBuffer[0] = PROTO_TYPE_FILL_LEVEL_REQ;
+        // TBD: make mutex wait time configurable/as param?
+        if(pdTRUE == xSemaphoreTake(requestMutex, portMAX_DELAY)) {
+            txBuffer[0] = PROTO_TYPE_FILL_LEVEL_REQ;
 
-        if(0 == packetizer->transmitData(1, txBuffer, wait)) {
-            if(pdPASS == xQueueReceive(rxPacketQueue, &rxPacketBuf, wait)) {
-                if((rxPacketBuf.len == 5) && (PROTO_TYPE_FILL_LEVEL_IND == rxPacketBuf.data[0])) {
-                    memcpy(&fillLevel, &rxPacketBuf.data[1], 4);
-                    ret = fillLevel;
+            if(0 == packetizer->transmitData(1, txBuffer, wait)) {
+                if(pdPASS == xQueueReceive(rxPacketQueue, &rxPacketBuf, wait)) {
+                    if((rxPacketBuf.len == 5) && (PROTO_TYPE_FILL_LEVEL_IND == rxPacketBuf.data[0])) {
+                        memcpy(&fillLevel, &rxPacketBuf.data[1], 4);
+                        ret = fillLevel;
+                    } else {
+                        ESP_LOGE(logTag, "Received answer isn't a proper fill level indication! len: %d, type: 0x%02x", rxPacketBuf.len, rxPacketBuf.data[0]);
+                        // TBD: distinctive error code
+                    }
                 } else {
-                    ESP_LOGE(LOG_TAG_FILL_PROTO, "Received answer isn't a proper fill level indication! len: %d, type: 0x%02x", rxPacketBuf.len, rxPacketBuf.data[0]);
+                    ESP_LOGE(logTag, "Receiving fill level timed out!");
                     // TBD: distinctive error code
                 }
             } else {
-                ESP_LOGE(LOG_TAG_FILL_PROTO, "Receiving fill level timed out!");
+                ESP_LOGE(logTag, "Couldn't send fill level request.");
+                // TBD: distinctive error code
+            }
+
+            if(pdFALSE == xSemaphoreGive(requestMutex)) {
+                ESP_LOGE(logTag, "Error occurred releasing the requestMutex.");
                 // TBD: distinctive error code
             }
         } else {
-            ESP_LOGE(LOG_TAG_FILL_PROTO, "Couldn't send fill level request.");
+            ESP_LOGE(logTag, "Error occurred acquiring the requestMutex.");
             // TBD: distinctive error code
         }
 
