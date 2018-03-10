b31400e06d2c29614a0f1f64d424603702b53b57
Manuel Wick <manuel@matronix.de>
2018-03-11 00:26:54 +0100

    Moved GIT_VERSION to the correct makefile

--- main/component.mk
+++ main/component.mk
@@ -4,3 +4,7 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 COMPONENT_ADD_INCLUDEDIRS := . include
+
+GIT_VERSION := $(shell git describe --abbrev=8 --dirty --always --tags)
+CFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+CXXFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
