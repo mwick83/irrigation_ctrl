60ca494f70123e5d37f6e60e76a17b3bb17491ec
Manuel Wick <manuel@matronix.de>
2019-04-25 22:50:12 +0200

    Removed wifi stp for deep sleep, just shut off the hard way
    Hopefully, this will reduce the offset from the desired sleep time
    (~1sec without it)

--- main/powerManager.cpp
+++ main/powerManager.cpp
@@ -179,7 +179,10 @@ bool PowerManager::gotoSleep(uint32_t ms)
         ESP_LOGI(logTag, "gotoSleep requested, but keep awake is set. Not going to sleep.");
     } else {
         // prepare for sleep
-        esp_wifi_stop(); // ignore return value, because we don't care if WiFi was up before
+        //esp_wifi_stop(); // ignore return value, because we don't care if WiFi was up before
+
+        // TBD: rtc_gpio_isolate for all pulled/driven I/Os for minimal power consumption
+        // TBD: setup ext0 wakeup for keepAwake input
 
         err = esp_sleep_enable_timer_wakeup(sleepUs);
         if(ESP_OK != err) ESP_LOGE(logTag, "Error setting up deep sleep timer.");
