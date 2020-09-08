d1092713d3bfa6dad9775014a802b47a7816d475
Manuel Wick <manuel@matronix.de>
2020-09-08 18:50:53 +0200

    Added missing files for SPIFFS config files

---
+++ main/include/fileConfig.h
@@ -0,0 +1,8 @@
+#ifndef FILE_CONFIG_H
+#define FILE_CONFIG_H
+
+static const char partlabelConfigStore[] = "cfg_store";
+static const char filepathConfigStore[] = "/cfg_store";
+static const char filenameIrrigationConfig[] = "/cfg_store/irrigationConfig.json";
+
+#endif /* FILE_CONFIG_H */
